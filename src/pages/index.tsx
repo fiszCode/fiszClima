8d666e840307aa528448465c5a8f7ec191040b2c
fiszCode <107449380+fiszCode@users.noreply.github.com>
2024-07-07 13:53:20 -0300

    Previo a la fragmentación del NavBar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,176 +2,34 @@
 ////////////////////
 
 // Importamos la dependencias básicas
-import React, { useState, useEffect, useRef} from 'react';
+import React, { useState} from 'react';
 import {css} from "@emotion/react"; 
 
 // Dependencias usadas por Material UI
-import {Box} from '@mui/material';
-import {styled, alpha} from '@mui/material/styles';
-import Tab from '@mui/material/Tab';
-import TabContext from '@mui/lab/TabContext';
-import TabList from '@mui/lab/TabList';
-import TabPanel from '@mui/lab/TabPanel';
-import dynamic from 'next/dynamic';
-import NavBar from '@/components/Navbar';
+import NavBar from '@/components/NavBar/NavBar';
 
 // Importamos los componentes propios
-import TemperaturaActual from "@/components/CajaClima/TemperaturaActual"; // C
-import BanderaPais from "@/components/CajaClima/BanderaPais"; // C
-import NombreCiudad from "@/components/CajaClima/NombreCiudad"; // C
-import Tarjeta from "@/components/CajaClima/Tarjeta"; // C
-import CajaClima from "@/components/CajaClima/CajaClima"; // C
-import Footer from "@/components/Footer"; // C
-
-const Mapa = dynamic(() => import('@/components/CajaClima/Mapa'), {
-    ssr: false, // Esto asegura que el mapa solo se renderice en el cliente
-  });
+import CajaClima from "@/components/CajaClima/CajaClima"; 
+import Footer from "@/components/Footer"; 
 
 // Componente principal
 function App() 
 {
-     // Creamos las variables donde vamos a guardar los datos recuperados de la API del clima
-     const [estadoPagina, setEstadoPagina] = useState("A");
      const [cityLat, setCityLat] = useState(0);
      const [cityLon, setCityLon] = useState(0);
-     const [temperaturaActual, setTemperaturaActual] = useState('');
-     const [termicaActual, setTermicaActual] = useState('');
-     const [minimaActual, setMinimaActual] = useState('');
-     const [maximaActual, setMaximaActual] = useState('');
-     const [presionActual, setPresionActual] = useState('');
-     const [humedadActual, setHumedadActual] = useState('');
-     const [visibilidadActual, setVisibilidadActual] = useState('');
-     const [vientoActual, setVientoActual] = useState('');
-     const [paisActual, setPaisActual] = useState('');
-     const [amanecerActual, setAmanecerActual] = useState(0);
-     const [atardecerActual, setAtardecerActual] = useState(0);
-     const [zonaHoraActual, setZonaHoraActual] = useState(0);
-     const [ciudadActual, setCiudadActual] = useState('');
-     const [climaActual, setClimaActual] = useState('');
-     const [climaActualID, setClimaActualID] = useState('');
-     const [climaActualICO, setClimaActualICO] = useState('');
-     const [climaActualDescripcion, setClimaActualDescripcion] = useState('');
-
-     // Agrega useEffect para llamar a obtenerDatosClima cuando cambien cityLat y cityLon
-     useEffect(() => { if (cityLat !== 0 && cityLon !== 0) {obtenerDatosClima()}}, [cityLat, cityLon]);
-     
-     // Función async encargada de obtener los datos de la API
-     const obtenerDatosClima = async () => 
-     {
-         console.log("obtenerDatosClima")
-         console.log("Valor actual LAT:")
-         console.log(cityLat)
-         console.log("Valor actual LON:")
-         console.log(cityLon)
-         const url = `https://api.openweathermap.org/data/2.5/weather?lat=${cityLat}&lon=${cityLon}&appid=72a8af477f23cc2e1c7eb81e9a142367`;
-         const response = await fetch(url);
-         const jsonData = await response.json();
-         console.log("Info obtenida en jsonData")
-         console.log(jsonData)
-         // Asignamos los valores individuales
-         setTemperaturaActual(jsonData?.main?.temp);
-         setTermicaActual(jsonData?.main?.feels_like);
-         setMinimaActual(jsonData?.main?.temp_min);
-         setMaximaActual(jsonData?.main?.temp_max);
-         setPresionActual(jsonData?.main?.pressure);
-         setHumedadActual(jsonData?.main?.humidity);
-         setVisibilidadActual(jsonData?.visibility); 
-         setVientoActual(jsonData?.wind?.speed);
-         setPaisActual(jsonData?.sys?.country);
-         setAmanecerActual(jsonData?.sys?.sunrise);
-         setAtardecerActual(jsonData?.sys?.sunset);
-         setZonaHoraActual(jsonData?.timezone);
-         setCiudadActual(jsonData?.name);
-         setClimaActual(jsonData?.weather[0]?.main);
-         setClimaActualID(jsonData?.weather[0]?.id);
-         setClimaActualICO(jsonData?.weather[0]?.icon);
-         setClimaActualDescripcion(jsonData?.weather[0]?.description);
-         setEstadoPagina("C");
-     }
-     
-     const [TabValue, setTabValue] = React.useState('2');
-     const handleTabChange = (event: React.SyntheticEvent, newTabValue: string) => {
-     setTabValue(newTabValue);} 
-
-     // Agrega un estado para la clave de actualización del mapa
-     const [mapKey, setMapKey] = useState(Date.now());
-     // Incrementa la clave cuando cambien cityLat y cityLon
-     useEffect(() => {setMapKey(Date.now())}, [cityLat, cityLon]);
-
-     // Return de la página 
-     if (estadoPagina !== "error") 
-     {
-         // Formulario de coordenadas + información del clima (sólo si está disponible)
-         return (
-             <div>
-                 <NavBar onCityClick={({ lon, lat }) =>{
-                        setCityLon(lon)
-                        setCityLat(lat)
-                  }} />
-                 <div css={cajaPadre}>
-                 {estadoPagina === "C" ? (
-                 
-                 <div css={cajaClima}>
-                     <div css={filaClima}>
-                         <BanderaPais siglas={paisActual}/>
-                         <NombreCiudad ciudad={ciudadActual}/>
-                     </div>
-                     <div css={filaClima}>
-                         <Mapa key={mapKey} lat={cityLat} lng={cityLon}/>
-                     </div>
-                     <div css={filaClima}>
-                         <TemperaturaActual temperatura={temperaturaActual} termica={temperaturaActual} escala = "C" clima = {climaActual} climaDESC={climaActualDescripcion} climaID={climaActualID} climaICO={climaActualICO}/>
-                     </div>
-                     <Box sx={{ width: '100%', typography: 'body1' }}>
-                         <TabContext value={TabValue}>
-                             <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-                                 <TabList centered onChange={handleTabChange} aria-label="lab API tabs example">
-                                     <Tab label={<span style={{ fontWeight: 'bold'}}>Semanal</span>} value="1" disabled />
-                                     <Tab label={<span style={{ fontWeight: 'bold', color: '#084b8f' }}>Amanecer/Atardecer</span>} value="2" />
-                                     <Tab label={<span style={{ fontWeight: 'bold', color: '#084b8f' }}>+ Info</span>} value="3" />
-                                 </TabList>
-                             </Box>
-                             <TabPanel value="1">Hola, soy un placeholder</TabPanel>
-                             <TabPanel value="2">
-                                 <div css={filaClima}>
-                                     <Tarjeta tipo={1} titulo = "" descripcion= "Hora actual" extra ={zonaHoraActual}/>
-                                     <Tarjeta tipo={2} titulo = {amanecerActual} descripcion= "Amanecer" extra ={zonaHoraActual}/>
-                                     <Tarjeta tipo={3} titulo = {atardecerActual} descripcion= "Anochecer" extra ={zonaHoraActual}/>
-                                 </div>
-                             </TabPanel>
-                             <TabPanel value="3">
-                                 <div css={filaClima}>
-                                     <Tarjeta tipo={4} titulo = {visibilidadActual} descripcion= "Visibilidad" extra = ""/>
-                                     <Tarjeta tipo={5} titulo = {humedadActual + "%"} descripcion= "Humedad" extra = ""/>
-                                     <Tarjeta tipo={6} titulo = {vientoActual} descripcion= "Viento" extra = ""/>
-                                 </div>
-                             </TabPanel>
-                         </TabContext>
-                     </Box>
-                     </div>
-                     
-                     ):(<div></div>)
-                    }    
-             <Footer/>
-             <CajaClima ciudadLat={cityLat} ciudadLon={cityLon}/>
+     return (
+         <div>
+             <NavBar onCityClick={({ lon, lat }) =>{setCityLon(lon);setCityLat(lat)}}/>
+             <div css={cajaPadre}>
+                 <CajaClima ciudadLat={cityLat} ciudadLon={cityLon}/>
+                 <Footer/>
              </div>
-             </div>
-         );
-     }
-     if (estadoPagina === "error") 
-     {
-         return (<div>No se que paso papu 🤌</div>);
-     }
+         </div>
+     );
 }
-
 export default App;
 
 // CSS ---------------------
-// Toda esta mierda podria estar en style.ts y ser reutilizable haciendo un export/import en cualquier otro archivito de mierda
-
-//const theme = useTheme();
-//const appBarHeight = theme.mixins.toolbar.minHeight; // Esto obtiene la altura mínima de AppBar
-
 const cajaPadre = css`
      width: 800px;
      margin: 0 auto;
@@ -180,17 +38,4 @@ const cajaPadre = css`
      padding-top:  75px; 
      border-radius: 10px;
      text-align: center;
-`;
-
-const cajaClima = 
-css`
-     display: flex;
-     flex-direction: column;
-     width: 800px;
-     max-width: 800px;
-`;
-const filaClima = 
-css`
-     display: flex;
-     flex-direction: row;
 `;
